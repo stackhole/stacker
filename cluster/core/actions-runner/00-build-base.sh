823cfa661efeaeb746dabc6b0d5dab9cacd69ba3
kgancarz <kyle.gancarz@gmail.com>
2022-04-02 17:37:37 -0400

    update runner bootstrap code

---
+++ cluster/core/actions-runner/00-build-base.sh
@@ -0,0 +1,1 @@
+docker build -f Dockerfile-base . -t ubuntu-base:latest
--- cluster/core/istio/install-istio.sh
+++ cluster/core/istio/install-istio.sh
@@ -1,3 +1,4 @@
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$ISTIO_VERSION TARGET_ARCH=x86_64 sh -
 istioctl install --set profile=minimal -y
+export PATH="$PATH:/actions-runner/_work/stacker/stacker/istio-$ISTIO_VERSION/bin"
 kubectl label namespace default istio-injection=enabled
