1c99da8ec3b9896ff44aade8a3e25e35fc11fa6a
kgancarz <kyle.gancarz@gmail.com>
2022-04-02 21:07:23 -0400

    push code

--- cluster/core/istio/install-istio.sh
+++
@@ -1,4 +0,0 @@
-curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$ISTIO_VERSION TARGET_ARCH=x86_64 sh -
-istioctl install --set profile=minimal -y
-export PATH="$PATH:/actions-runner/_work/stacker/stacker/istio-$ISTIO_VERSION/bin"
-kubectl label namespace default istio-injection=enabled
