9a66feec302ede43961be6e867c507d371803076
kgancarz <kyle.gancarz@gmail.com>
2022-04-02 23:04:33 -0400

    Update index.js

--- cluster/service/index.js
+++ cluster/service/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
 app.get('/hello', (req, res) => {
   res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
