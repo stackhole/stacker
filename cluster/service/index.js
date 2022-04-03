1d7f5e21b73271ea11db4a22a82bb67659eec384
kgancarz <kyle.gancarz@gmail.com>
2022-04-03 01:29:01 -0400

    update

--- cluster/service/index.js
+++ cluster/service/index.js
@@ -1,10 +1,38 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+const { Pool } = require("pg");
+
+const credentials = {
+  user: "postgres",
+  host: "database-service.default.svc.cluster.local",
+  database: "sample",
+  password: "postgres",
+  port: 5432,
+};
+
+const pool = new Pool(credentials)
+
+
+
 
 app.get('/hello', (req, res) => {
   res.send('hello world')
 })
+
+app.get('/db', (req, res) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+        if (error) {
+          console.log(error);
+          throw error
+        }
+        response.status(200).json(results.rows)
+      })
+  })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
