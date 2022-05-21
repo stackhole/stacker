9d7f1ac42aac94202423783163543df788f9a07e
kgancarz <kyle.gancarz@gmail.com>
2022-05-20 22:16:05 -0400

    Update PasswordPage.tsx

--- cluster/identity/identity-ui/src/pages/password/PasswordPage.tsx
+++ cluster/identity/identity-ui/src/pages/password/PasswordPage.tsx
@@ -1,6 +1,6 @@
 import { Button, TextField } from '@mui/material';
 import axios from 'axios';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import './PasswordPage.css';
 
 
