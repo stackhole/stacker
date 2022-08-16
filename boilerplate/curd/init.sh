d2b3de50a32a2e96b26ee6e84676bad3827c6ec3
kgancarz <kyle.gancarz@gmail.com>
2022-08-15 23:30:02 -0400

    Update init.sh

--- boilerplate/curd/init.sh
+++ boilerplate/curd/init.sh
@@ -4,6 +4,6 @@ GITHUB_REPOSITORY=stackhole/user
 REPOSITORY_NAME=${GITHUB_REPOSITORY#*/}
 UPPER_REPOSITORY_NAME=${REPOSITORY_NAME^}
 
-for f in $(find /tmp -name '*.pdf' -or -name '*.doc'); 
+for f in $(find . -type f); 
 do sed -i -e "s/curd/${REPOSITORY_NAME#*/}/g" $f && sed -i -e "s/Curd/${UPPER_REPOSITORY_NAME#*/}/g" $f;
 done
