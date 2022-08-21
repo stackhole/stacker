646a9cbb3c88e243f20b1b2cdf5af88e7217a78b
kgancarz <kyle.gancarz@gmail.com>
2022-08-20 22:59:45 -0400

    Update registry-config.sh

--- cluster/03-istio/registry-config.sh
+++ cluster/03-istio/registry-config.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-for f in $(find . -type f); 
-do sed -i -e "s/localhost\:32000/registry.container-registry.svc.cluster.local\:32000/g" $f;
-done
+#for f in $(find . -type f); 
+#do sed -i -e "s/localhost\:32000/registry.container-registry.svc.cluster.local\:32000/g" $f;
+#done
+
+
+sed -i -e "s/localhost\:32000/registry.container-registry.svc.cluster.local\:32000/g" skaffold.yaml
