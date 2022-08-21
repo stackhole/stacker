7429e51b133637567918cd1a3e17a7e6181f016a
kgancarz <kyle.gancarz@gmail.com>
2022-08-20 23:52:06 -0400

    updates

--- cluster/03-istio/deploy.sh
+++ cluster/03-istio/deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
-docker build -t istio-deploy .
-docker run istio-deploy
+
+ISTIO_NAMESPACE=default
+APP_DOMAIN=api
+DOMAIN_ROOT=stackhole.com
+ORGANIZATION=stackhole
+
+
+istioctl install --set profile=default -y
+kubectl label namespace $ISTIO_NAMESPACE istio-injection=enabled
+
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj "/O=$ORGANIZATION/CN=$DOMAIN_ROOT" -keyout $DOMAIN_ROOT.key -out $DOMAIN_ROOT.crt
+openssl req -out $APP_DOMAIN.$DOMAIN_ROOT.csr -newkey rsa:2048 -nodes -keyout $APP_DOMAIN.$DOMAIN_ROOT.key -subj "/CN=$APP_DOMAIN.$DOMAIN_ROOT/O=$ORGANIZATION"
+openssl x509 -req -sha256 -days 365 -CA $DOMAIN_ROOT.crt -CAkey $DOMAIN_ROOT.key -set_serial 1 -in $APP_DOMAIN.$DOMAIN_ROOT.csr -out $APP_DOMAIN.$DOMAIN_ROOT.crt
+kubectl create -n istio-system secret tls $APP_DOMAIN-credential --key=$APP_DOMAIN.$DOMAIN_ROOT.key --cert=$APP_DOMAIN.$DOMAIN_ROOT.crt
--- cluster/03-istio/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-istioctl install --set profile=default -y
-kubectl label namespace $ISTIO_NAMESPACE istio-injection=enabled
-
-openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj "/O=$ORGANIZATION/CN=$DOMAIN_ROOT" -keyout $DOMAIN_ROOT.key -out $DOMAIN_ROOT.crt
-openssl req -out $APP_DOMAIN.$DOMAIN_ROOT.csr -newkey rsa:2048 -nodes -keyout $APP_DOMAIN.$DOMAIN_ROOT.key -subj "/CN=$APP_DOMAIN.$DOMAIN_ROOT/O=$ORGANIZATION"
-openssl x509 -req -sha256 -days 365 -CA $DOMAIN_ROOT.crt -CAkey $DOMAIN_ROOT.key -set_serial 1 -in $APP_DOMAIN.$DOMAIN_ROOT.csr -out $APP_DOMAIN.$DOMAIN_ROOT.crt
-kubectl create -n istio-system secret tls $APP_DOMAIN-credential --key=$APP_DOMAIN.$DOMAIN_ROOT.key --cert=$APP_DOMAIN.$DOMAIN_ROOT.crt
