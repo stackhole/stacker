860b2e2b9b86c7a52d32ee1ce4f1ac5fad5e7410
kgancarz <kyle.gancarz@gmail.com>
2022-08-20 23:38:45 -0400

    updates

--- cluster/03-istio/deploy.sh
+++ cluster/03-istio/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-skaffold run
-docker run localhost:32000/istio-deploy
+docker build -t istio-deploy .
+docker run istio-deploy
--- cluster/03-istio/registry-config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#for f in $(find . -type f); 
-#do sed -i -e "s/localhost\:32000/registry.container-registry.svc.cluster.local\:32000/g" $f;
-#done
-
-
-sed -i -e "s/localhost\:32000/registry.container-registry.svc.cluster.local\:32000/g" skaffold.yaml
-sed -i -e "s/localhost\:32000/registry.container-registry.svc.cluster.local\:32000/g" Dockerfile
