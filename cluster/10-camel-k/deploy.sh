6f4363537708003ea717f93c2d661c89c7358fac
Kyle Gancarz <kyle.gancarz@gmail.com>
2022-10-02 04:50:58 +0000

    update

--- cluster/10-camel-k/deploy.sh
+++ cluster/10-camel-k/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NAMESPACE=camel-k-test
+NAMESPACE=camel-k
 ORGANIZATION=stackhole
 kubectl create namespace $NAMESPACE || true
 kamel install -n $NAMESPACE --force --olm=false --registry localhost:32000 --organization $ORGANIZATION --registry-insecure true
